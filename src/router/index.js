b36418adc7e971251b355413b3c3e81b7dd5ddbf
jaeryeol <ryeol22e@gmail.com>
2022-10-23 21:23:51 +0900

    상품상세, bo 개발

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,12 @@ const routes = [
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition)  {
+	return {
+		top : 0,
+	};
+  }
 })
 
 router.beforeEach((to, from, next)=> {
---
+++ src/store/useStoreBackOffice.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useStoreBo = defineStore('useStoreBo', {
+	state : ()=> ({
+
+	}),
+	getters : {
+
+	},
+	actions : {
+
+	}
+});
