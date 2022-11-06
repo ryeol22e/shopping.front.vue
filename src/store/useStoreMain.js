e69b470744357d7816c881d49abdd7074edeffbe
jaeryeol <ryeol22e@gmail.com>
2022-11-06 19:10:29 +0900

    메인 개발

--- src/store/useStoreMain.js
+++ src/store/useStoreMain.js
@@ -3,12 +3,18 @@ import axios from "axios";
 
 export default defineStore('main', {
 	state : ()=> ({
-
+		bannerList : [],
 	}),
 	getters : {
-
+		getBannerList : state=> state.bannerList,
 	},
 	actions : {
-
+		async setBannerList(param) {
+			this.bannerList = await axios.get('/display/main/banner', {
+				params : param
+			})
+			.then(res=> res.data)
+			.catch(error=> console.log(error));
+		},
 	}
 })
