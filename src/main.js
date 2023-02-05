6b65d497f273fa6582b4dd2ed227fab5f7e919b6
jaeryeol <ryeol22e@gmail.com>
2023-02-05 20:06:04 +0900

    custom axios

--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import {useConfig} from './assets/js/config/useConfig.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// s : config set
-const config = useConfig();
-// e : config set
 
 // s : vue create
 const app = createApp(App);
 
 app.use(router);
-app.use(config.axiosConfig())
 app.use(createPinia());
 app.mount('#app');
 // e : vue create
