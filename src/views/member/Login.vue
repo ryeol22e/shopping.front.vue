372916181421aaaae9f0c5ae6f7d622e539fead7
jaeryeol <ryeol22e@gmail.com>
2023-02-11 23:33:30 +0900

    로그인 개선

--- src/views/member/Login.vue
+++ src/views/member/Login.vue
@@ -27,6 +27,10 @@
 	import useStoreMember from '@/store/useStoreMember.js';
 	import {useUtils} from '@/composables/useUtils.js';
 
+	if(!utils.isEmpty(sessionStorage.getItem('userInfo'))) {
+		router.push('/');
+	}
+	
 	const utils = useUtils();
 	const useCookie = utils.useCookie();
 	const useMember = useStoreMember();
@@ -38,7 +42,6 @@
 		memberId : remember ? memberId : '',
 		memberPassword : '',
 	});
-	
 	const loginProcess = async ()=> {
 		if(validate()) {
 			await useMember.loginProcess(data);
