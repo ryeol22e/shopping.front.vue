b330a0fa11476402c2bf093607b330b58e474a23
jaeryeol <ryeol22e@gmail.com>
2024-01-11 21:58:22 +0900

    storeVariable

--- src/stores/useStoreVariable.js
+++ src/stores/useStoreVariable.js
@@ -1,7 +1,16 @@
 import { defineStore } from 'pinia';
 
 export const useStoreVariable = defineStore('useStoreVariable', {
-	state: () => ({}),
-	getters: {},
-	actions: {},
+	state: () => ({
+		reqParam: {},
+	}),
+	getters: {
+		getReqParam: (state) => state.reqParam,
+	},
+	actions: {
+		setReqParam(data = {}) {
+			console.log(data);
+			this.reqParam = data;
+		},
+	},
 });
